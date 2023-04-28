21fa61c9668b4ad4b458be6ad645e61f8167158b
Ahmed Abuelenin <ahprogsolutions@gmail.com>
2023-04-28 19:06:20 +0300

    Feat(Swiper Pagination): Added swiper pagination bullets

--- scripts/swiper-script.js
+++ scripts/swiper-script.js
@@ -4,11 +4,23 @@ const swiper = new Swiper('.swiper', {
     delay: 5000,
     disableOnInteraction: false
   },
+  pagination: {
+    el: '.custom-swiper-pagination',
+    type: 'bullets',
+    enabled: true,
+    horizontalClass: 'swiper-pagination-horizontal',
+    bulletClass: 'swiper-pagination-bullet',
+    bulletActiveClass: 'swiper-pagination-bullet-active'
+  },
   breakpoints: {
     // when window width is >= 501px
     501: {
       slidesPerView: 3,
-      spaceBetween: 20
+      spaceBetween: 20,
+      pagination: {
+        enabled: false,
+        paginationDisabledClass: 'swiper-pagination-disabled'
+      }
     }
   },
   spaceBetween: 20,
