45c289fae3d19e1d60af762e2abfa5ad86aea9df
Ahmed Abuelenin <ahprogsolutions@gmail.com>
2023-04-28 16:36:54 +0300

    Enhance(Popup MenuList): Made Menu enhancement for popup list to be like a modal

--- scripts/action-script.js
+++ scripts/action-script.js
@@ -1,12 +1,27 @@
 const menu = document.querySelector('.menu-icon')
+const navigation = document.querySelector('.navigation')
+const overlay = document.querySelector('.overlay')
+
+function toggleActiveClass() {
+  menu.classList.toggle('clicked')
+  navigation.classList.toggle('active')
+  overlay.classList.toggle('active')
+}
+
 menu.addEventListener('click', function () {
   const isClicked = menu.classList.contains('clicked')
-  menu.classList.toggle('clicked')
+  toggleActiveClass()
 
   if (!isClicked) {
     menu.src = './images/icon-close.svg'
     return
   }
+
+  menu.src = './images/icon-burger.svg'
+})
+
+overlay.addEventListener('click', function () {
+  toggleActiveClass()
 
   menu.src = './images/icon-burger.svg'
 })
